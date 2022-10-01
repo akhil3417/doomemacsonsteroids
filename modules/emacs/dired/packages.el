a7dc46a6b90e4a82b463ad60f3846eca8e260e86
Alex <hellosimon1103@hotmail.com>
2022-10-01 14:12:50 +0800

    feat!(dired): use dirvish by default
    BREAKING CHANGE: Deprecate `+ranger` flag, use package dirvish by
    default and rewrite the module based on it.  Doing so we can remove
    these packages and their associated hacks: `all-the-icons-dired`,
    `dired-rsync`, `fd-dired`, `dired-git-info`, `ranger`. See file log
    below for details.
    
    purcell/diredfl@f9140b2c4215 -> purcell/diredfl@62b559e1d6b6
    alexluigit/dirvish@73dcaa404da9 -> alexluigit/dirvish@11a3bec247b9
    
    * modules/ui/vc-gutter/config.el(dired-mode-hook, diff-hl-dired-mode):
      when dired module is enabled, use `vc-state` from dirvish to render
      the vc gutter instead of diff-hl-dired-mode, doing so we get better
      performance and integration with the dired module.
    * modules/emacs/dired/autoload.el: delete this file since the commands
      in it are no longer used.
    * modules/emacs/dired/doctor.el: add deprecate warning for `+ranger`
      flag
    * modules/emacs/dired/README.org: rewrite based on the new features
    * modules/emacs/dired/config.el:
      - (dirvish-mode-line-format, dired-hide-details-hide-symlink-targets):
        set `dired-hide-details-hide-symlink-targets` to t, show symlink
        target on mode-line instead
      - (dired-listing-switches, +dired-disable-gnu-ls-flags-maybe-h):
        dirvish has inbuilt gnuls check, so the setup for
        `dired-listing-switches` can be simplified as such and the
        `+dired-disable-gnu-ls-flags-maybe-h` can be safely removed.
      - (+dired/quit-all): dirvish has a fine-tuned machinery for closing
        dired buffers, so replace this command with `dirvish-quit`.  One can
        set `dirvish-reuse-session` to nil to get similar behavior to
        `+dired/quit-all`.
      - (dired-rsync): replace `dired-rsync` with `dirvish-rsync`
      - (+ranger): deprecate this flag.
      - (dirvish-attributes, all-the-icons-dired): remove package
        `all-the-icons-dired`, use `all-the-icons` attribute in dirvish to
        implement the `+icons` flag.
      - (fd-dired): use the better alternative dirvish-fd to replace this
        command/package.
      - (dirvish-attributes, dired-git-info-mode): use `git-msg` attribute
        from dirvish to replace dired-git-info-mode.
      - (dirvish-attributes): add `collapse` attribute for `+dirvish` flag
      - (dirvish-attributes): add `subtree-state` attribute for `+icons`
        flag
      - (dirvish-mode-map): introduce a few new keybindings such as
        `dirvish-subtree-toggle`
      - (dirvish-attributes, dirvish-use-header-line,
         dirvish-mode-line-format, dirvish-hide-cursor,
         dirvish-hide-details): support classic dired UI when +dirvish is
         not enabled.
    
    Fix: #6562
    Replace: #6568
    Ref: https://github.com/alexluigit/dirvish/blob/a877ba816f907daea2d86bd10754bec6484e753a/dirvish-extras.el#L172-L190
    Ref: alexluigit/dirvish#34
    Ref: https://github.com/alexluigit/dirvish/blob/main/docs/COMPARISON.org#find-dired--fd-dired
    Ref: https://github.com/alexluigit/dirvish/blob/main/docs/COMPARISON.org#all-the-icons-dired
    Ref: https://github.com/alexluigit/dirvish/blob/main/docs/COMPARISON.org#dired-git-info--diff-hl--dired-k
    Ref: https://github.com/alexluigit/dirvish/blob/main/docs/COMPARISON.org#dired-rsync

--- modules/emacs/dired/packages.el
+++ modules/emacs/dired/packages.el
@@ -1,14 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; emacs/dired/packages.el
 
-(package! diredfl :pin "f9140b2c42151dca669003d685c9f079b2e3dc37")
-(package! dired-git-info :pin "9461476a28a5fec0784260f6e318237c662c3430")
-(package! dired-rsync :pin "7940d9154d0a908693999b0e1ea351a6d365c93d")
-(when (modulep! +ranger)
-  (package! ranger :pin "2498519cb21dcd5791d240607a72a204d1761668"))
-(when (modulep! +dirvish)
-  (package! dirvish :pin "4fe9c00894304e99aca22ae4b6b656fe94b8f927"))
-(when (and (modulep! +icons)
-           (not (modulep! +dirvish)))
-  (package! all-the-icons-dired :pin "5e9b097f9950cc9f86de922b07903a4e5fefc733"))
-(package! fd-dired :pin "458464771bb220b6eb87ccfd4c985c436e57dc7e")
+(package! diredfl :pin "17e805763d57370c4eff2c92ed257b72eeb9f94a")
+(package! dirvish :pin "6e25b16e1c68d92f917189f15ac49e4ff480dce2")
