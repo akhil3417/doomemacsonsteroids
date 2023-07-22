f87f83824444b9b1411e0eddd8681c4dc38e7fe5
akhil3417 <akhilpratapsingh3417@gmail.com>
2023-07-22 09:30:26 +0530

    fix(vertico): add advice to open media files , using find file

--- modules/completion/vertico/config.el
+++ modules/completion/vertico/config.el
@@ -319,3 +319,34 @@ orderless."
   :hook (vertico-mode . vertico-posframe-mode)
   :config
   (add-hook 'doom-after-reload-hook #'posframe-delete-all))
+
+
+;; The problem this solves is that when you try to open an media file
+;; using find-file in Emacs, it would typically open the file in a
+;; text buffer rather than launching a media player to play the
+;; video. By adding the ak/find-file-handler function as advice to
+;; find-file, it intercepts the file opening process and checks if
+;; the file being opened has an MP4 extension. If it does, it starts
+;; a new mpv process to play the video file instead of opening it in
+;; a text buffer.
+
+(defvar ak/media-player "mpv"
+  "The media player to open media files.")
+
+(defvar ak/media-players
+  (cl-loop for ext in '("mp4" "mkv" "webm" "avi" "flv" "mov" "wmv" "m4b""mp3" "opus" "wav" "ogg")
+           collect (cons ext ak/media-player)))
+
+(defun ak/find-file-handler (orig-fun &rest args)
+  "Open media files with the associated media player based on file extension."
+  (let ((file (expand-file-name (car args))))
+    (if (and (stringp file)
+             (file-regular-p file))
+        (let* ((file-ext (downcase (file-name-extension file)))
+               (media-player (cdr (assoc file-ext ak/media-players))))
+          (if media-player
+              (start-process "ak/file-handler" nil media-player file)
+            (apply orig-fun args)))
+      (apply orig-fun args))))
+
+(advice-add #'find-file :around #'ak/find-file-handler)
