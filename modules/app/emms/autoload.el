af9dc43df690cab90b84d51d4874520bc4a6ad0e
akhil3417 <akhilpratapsingh3417@gmail.com>
2023-08-05 04:06:24 +0530

    refractor(emms): rewrite the whole emms again

--- modules/app/emms/autoload.el
+++ modules/app/emms/autoload.el
@@ -1,5 +1,7 @@
 ;;; app/emms/autoload.el -*- lexical-binding: t; -*-
 
+(defvar +volume-muted nil "Flag to track the muted state")
+
 ;;;###autoload
 (defun +emms/mpd-start-music-daemon ()
   (interactive)
@@ -28,3 +30,141 @@
   (+emms/mpd-kill-music-daemon)
   (+emms/mpd-start-music-daemon)
   (message "MPD Restarted!"))
+
+
+  (defun +emms-toggle-time-display ()
+    "Toggle the display of time information in the modeline"
+    (interactive)
+    (if emms-playing-time-display-p
+        (emms-playing-time-disable-display)
+      (emms-playing-time-display-mode)))
+
+  (defun +emms-select-song ()
+    "Select and play a song from the current EMMS playlist."
+    (interactive)
+    (with-current-emms-playlist
+      (emms-playlist-mode-center-current)
+      (let* ((current-line-number (line-number-at-pos))
+             (lines (cl-loop
+                     with min-line-number = (line-number-at-pos (point-min))
+                     with buffer-text-lines = (split-string (buffer-string) "\n")
+                     with lines = nil
+                     for l in buffer-text-lines
+                     for n = min-line-number then (1+ n)
+                     do (push (cons l n)
+                              lines)
+                     finally return (nreverse lines)))
+             (selected-line (completing-read "Song: " lines)))
+        (when selected-line
+          (let ((line (cdr (assoc selected-line lines))))
+            (goto-line line)
+            (emms-playlist-mode-play-smart)
+            (emms-playlist-mode-center-current))))))
+
+  (defun track-title-from-file-name (file)
+    "For using with EMMS description functions. Extracts the track
+title from the file name FILE, which just means a) taking only
+the file component at the end of the path, and b) removing any
+file extension."
+    (with-temp-buffer
+      (save-excursion (insert (file-name-nondirectory (directory-file-name file))))
+      (ignore-error 'search-failed
+        (search-forward-regexp (rx "." (+ alnum) eol))
+        (delete-region (match-beginning 0) (match-end 0)))
+      (buffer-string)))
+
+  (defun my-emms-track-description (track)
+    "Return a description of TRACK, for EMMS, but try to cut just
+the track name from the file name, and just use the file name too
+rather than the whole path."
+    (let ((artist (emms-track-get track 'info-artist))
+          (title (emms-track-get track 'info-title)))
+      (cond ((and artist title)
+             ;; Converting the artist/title to a string works around a bug in `emms-info-exiftool'
+             ;; where, if your track name is a number, e.g. "1999" by Jeroen Tel, then it will be an
+             ;; integer type here, confusing everything.
+             ;;
+             ;; I would fix the bug properly and submit a patch but I just cannot be bothered to
+             ;; figure out how to do that.
+             (concat (format "%s" artist) " - " (format "%s" title)))
+            (title title)
+            ((eq (emms-track-type track) 'file)
+             (track-title-from-file-name (emms-track-name track)))
+            (t (emms-track-simple-description track)))))
+
+  (setq emms-track-description-function 'my-emms-track-description)
+
+(defun my/get-volume ()
+  (* my/volume-step (round (string-to-number
+                                ;; (shell-command-to-string "awk -F\"[][]\" '/dB/ { print $2 }' <(amixer sget Master)"))
+                                ;; (shell-command-to-string "awk -F\"[][]\" '/dB/ { print $2 }' <(pamixer --get-volume)"))
+                                (shell-command-to-string "pamixer --get-volume"))
+                               my/volume-step)))
+
+(defun my/set-volume (level)
+  (interactive "nVolume level: ")
+  (let ((clipped-level
+         (cond ((< level +sys-vol-min) +sys-vol-min)
+               ((> level +sys-vol-max) +sys-vol-max)
+               (t level))))
+    (save-window-excursion
+      (shell-command
+       ;; (format "amixer set Master %s%% &" clipped-level) nil nil))))
+       (format "pamixer --set-volume %s &" clipped-level) nil nil))))
+
+
+(defun +volume-toggle-mute ()
+  "Toggle the mute state of the volume"
+  (interactive)
+  (setq +volume-muted (not +volume-muted))
+  (save-window-excursion
+  (if +volume-muted
+      (async-shell-command "pamixer --mute &")
+    (async-shell-command "pamixer --unmute &"))))
+
+;;;###autoload
+(defun +volume-step-change (delta)
+  (+set-volume (+ delta (+get-volume))))
+
+;;;###autoload
+(defun +volume-increase ()
+  (interactive)
+  (+volume-step-change +sys-vol-step))
+
+;;;###autoload
+(defun +volume-decrease ()
+  (interactive)
+  (+volume-step-change (- +sys-vol-step)))
+
+;; ;;in case you are'nt using mpd server you can use this hack to get notifications
+;; ; choose D-Bus to disseminate messages, if it is running.
+;; (cond
+;;  ;; test to see if D-Bus notifications are available
+;;  ((if (and (require 'dbus nil t)
+;; 	   (dbus-ping :session "org.freedesktop.Notifications"))
+;;       (progn
+;; 	(setq notify-method 'notify-via-dbus-notifications)
+;; 	(require 'notifications))))
+;;  ;; could use the message system otherwise
+;;  (t (setq notify-method 'notify-via-message)))
+
+;; (defun notify-via-notifications (title msg icon)
+;;   "Send notification with TITLE, MSG via `D-Bus'."
+;;   (notifications-notify
+;;    :title title
+;;    :body msg
+;;    :app-icon icon
+;;    :urgency 'low))
+
+;; (defun notify-via-messages (title msg)
+;;   "Send notification with TITLE, MSG to message."
+;;   (message "APPOINTMENT: %s" msg))
+
+;; (defun emms-notifications-dbus (track-name)
+;;   "Share track name via `D-Bus'."
+;;   (let ((icon "/usr/share/icons/gnome/24x24/categories/applications-multimedia.png"))
+;;     (notify-via-notifications "EMMS is now playing:" track-name icon)))
+
+;; (defun emms-notifications-message (track-name)
+;;   "Share track name via Emacs minibuffer."
+;;   (message "EMMS is now playing: %s" track-name))
--- modules/app/emms/config.el
+++ modules/app/emms/config.el
@@ -1,13 +1,157 @@
 ;;; app/emms/config.el -*- lexical-binding: t; -*-
 
 (use-package! emms
-  :defer t
+  ;; :defer t
+  ;; :commands (emms)
   :init
   (setq emms-directory (concat doom-data-dir "emms")
         emms-cache-file (concat doom-cache-dir "emms"))
   :config
   (emms-all)
   (emms-default-players)
+  ;; save on quit and recover on startup
+  ;; (require 'emms-history)
+  ;; (emms-history-load)
+  (require 'emms-player-mpd)
+  (add-to-list 'emms-info-functions 'emms-info-mpd)
+  (setq emms-source-file-default-directory "~/Music/"
+        emms-player-mpd-music-directory "~/Music/" ;; fixes a annoying bug
+        emms-mode-line-format "「%s」"
+        emms-playlist-buffer-name "*Music*"
+        emms-info-asynchronously t
+        emms-show-format "NP: %s"
+        emms-source-file-directory-tree-function 'emms-source-file-directory-tree-find ;; gnu "find" required
+        emms-browser-covers 'emms-browser-cache-thumbnail-async
+        emms-player-list '(emms-player-mpd) ;; mpv is too silent
+        ;; emms-player-mpv-environment '("PULSE_PROP_media.role=music")
+        ;; emms-player-mpv-parameters '("--quiet" "--really-quiet" "--no-audio-display" "--force-window=no" "--vo=xv");; can try --vo=null but does'nt play well at times
+        emms-browser-thumbnail-small-size 64
+        emms-browser-thumbnail-medium-size 128) ;; defaults are too big
+
+  (emms-browser-make-filter "all" #'ignore)
+  (emms-browser-make-filter "recent"
+                            (lambda (track) (< 30
+                                          (time-to-number-of-days
+                                           (time-subtract (current-time)
+                                                          (emms-info-track-file-mtime track))))))
+  (emms-browser-set-filter (assoc "all" emms-browser-filters))
+  ;; libre-fm
+  ;; (emms-librefm-scrobbler-enable)
+
+  (emms-playing-time-disable-display)
+
+  (defvar emms-browser-info-title-format "%i%n")
+  (defvar emms-browser-playlist-info-title-format
+    emms-browser-info-title-format)
+
+;; Show all tracks played in the last month:
+(emms-browser-make-filter
+ "last-month" (emms-browser-filter-only-recent 30))
+
+;; After executing the above commands, you can use M-x emms-browser-show-all, emms-browser-show-80s, etc to toggle between different collections. Alternatively you can use ’<’ and ’>’ to cycle through the available filters.
+;; The second argument to make-filter is a function which returns t if a single track should be filtered. You can write your own filter functions to check the type of a file, etc.
+;; Show only tracks not played in the last year:
+
+(emms-browser-make-filter "not-played"
+ (lambda (track)
+  (not (funcall (emms-browser-filter-only-recent 365) track))))
+
+;To show a ’no cover’ image for albums ;;which don’t have a cover, add the
+;;following code to your .emacs:
+;; Suggested sizes are 100x100 for small, and 200x200 for medium.
+  ;; (setq emms-browser-default-covers
+  ;;   (list "/path/to/cover_small.jpg" nil nil))
+
+  (defadvice emms-browser-next-mapping-type
+      (after no-album (current-mapping))
+    (when (eq ad-return-value 'info-album)
+      (setq ad-return-value 'info-title)))
+
+  (defun toggle-album-display ()
+    (if (string= emms-browser-current-filter-name "singles")
+        (ad-activate 'emms-browser-next-mapping-type)
+      (ad-deactivate 'emms-browser-next-mapping-type)))
+
+  (add-hook 'emms-browser-filter-changed-hook 'toggle-album-display)
+
+  (defun my/tick-symbol (x)
+    "Return a tick if X is true-ish."
+    (if x "x" " "))
+
+  (defun my/emms-player-status ()
+    "Return the state of the EMMS player: `not-active', `playing', `paused' or `dunno'.
+
+Modeled after `emms-player-pause'."
+    (cond ((not emms-player-playing-p)
+           ;; here we should return 'not-active.  The fact is that
+           ;; when i change song, there is a short amount of time
+           ;; where we are ``not active'', and the hydra is rendered
+           ;; always during that short amount of time.  So we cheat a
+           ;; little.
+           'playing)
+
+          (emms-player-paused-p
+           (let ((resume (emms-player-get emms-player-playing-p 'resume))
+                 (pause (emms-player-get emms-player-playing-p 'pause)))
+             (cond (resume 'paused)
+                   (pause  'playing)
+                   (t      'dunno))))
+          (t (let ((pause (emms-player-get emms-player-playing-p 'pause)))
+               (if pause 'playing 'dunno)))))
+
+  (defhydra hydra-emms (:hint nil)
+    "
+%(my/emms-player-status) %(emms-track-description (emms-playlist-current-selected-track))
+
+^Volume^         ^Controls^       ^Playback^              ^Misc^
+^^^^^^^^----------------------------------------------------------------
+_d_: inc        _n_: next         _r_: repeat one [% s(my/tick-symbol emms-repeat-track)]     _t_oggle modeline
+_o_: dec        _c_: prev         _R_: repeat all [% s(my/tick-symbol emms-repeat-playlist)]     _T_oggle only time
+_v_: sysvol     _a_: seek bw      _#_: shuffle            _s_elect
+_D_: sysvol+    _h_: seek fw      _%_: sort               _g_oto EMMS buffer
+_O_: sysvol-    _SPC_: play/pause _m_: mute/unmute       _l_yrics
+^ ^             _DEL_: restart                            _L_yrics select
+  "
+    ;; ("v" sndio-win-open :exit t)
+    ;; ("d" emms-volume-raise) ;; TODO ;; needs fix to work with mpc
+    ;; ("o" emms-volume-lower)
+    ;; in the meanwhile
+    ("d" simple-mpc-increase-volume)
+    ("o" simple-mpc-decrease-volume)
+    ("D" my/volume-increase)
+    ("O" my/volume-decrease)
+    ("m" +volume-toggle-mute)
+    ("v" my/set-volume)
+    ("n" emms-next)
+    ("c" emms-previous) ;;blame my keyboard layout for not beign mnemonic
+    ("a" emms-seek-backward)
+    ("h" emms-seek-forward)
+    ("SPC" emms-pause)
+    ("DEL" (emms-player-seek-to 0))
+    ("<backspace>" (emms-player-seek-to 0))
+    ("r" emms-toggle-repeat-track)
+    ("R" emms-toggle-repeat-playlist)
+    ("#" emms-shuffle)
+    ("%" emms-sort)
+    ("t" (progn (+emms-toggle-time-display)
+                (emms-mode-line-toggle)))
+    ("T" +emms-toggle-time-display)
+    ("s" +emms-select-song)
+    ("g" (progn (emms)
+                (with-current-emms-playlist
+                  (emms-playlist-mode-center-current))))
+    ("l" my/emms-current-lyrics :exit t)
+    ("L" my/versuri-select :exit t)
+
+    ("q" nil :exit t))
+
+(map! :leader
+      (:prefix ("y" . "EMMS audio player")
+       :desc "Go to emms playlist" "p" #'emms-playlist-mode-go
+       :desc "Emms Mpd start deamon" "s" #'+emms/mpd-start-music-daemon
+       :desc "Emms Mpd update deamon" "u" #'+emms/mpc-update-database
+       :desc "hydra emms body " "h" #'hydra-emms/body))
+
   (map! :map emms-playlist-mode-map
         :localleader
         "l" #'emms-toggle-repeat-playlist
@@ -16,3 +160,10 @@
         "t" #'emms-toggle-repeat-track
         "s" #'emms-playlist-save
         "m" #'emms-shuffle))
+
+
+(defun +emms-bind-key (key command)
+  (unless (local-key-binding key)
+    (define-key (current-local-map) key command)))
+
+(+emms-bind-key (kbd "C-c-e") 'hydra-emms/body)
