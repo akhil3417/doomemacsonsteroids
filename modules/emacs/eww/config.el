d26569c913b2921618f47b2fe081c3ea4d104201
akhil3417 <akhilpratapsingh3417@gmail.com>
2023-08-06 15:26:26 +0530

    refractor(eww): add keybindings

--- modules/emacs/eww/config.el
+++ modules/emacs/eww/config.el
@@ -19,11 +19,31 @@
         eww-retrieve-command nil) ;; NOTE set to 'nil in case it messes up with paragraph spacing
         ;; eww-retrieve-command '("wget" "--quiet" "--output-document=-")) ;; NOTE set to 'nil in case it messes up with paragraph spacing
 
+  ;;NOTE following keybinds try to mimic qutebrowser
   (map! :map eww-mode-map
         :ni "C-<return>" #'+eww-open-in-other-window
-        :n "yy" #'+eww-copy-current-url
-        :n "zk" #'+eww-increase-font-size
-        :n "zj" #'+eww-decrease-font-size
+        :n "yp" #'eww-copy-page-url
+        :n "yl" #'link-hint-copy-link-at-point
+        :n "yo" #'org-eww-copy-for-org-mode
+        :n "+" #'+eww-increase-font-size
+        :n "-" #'+eww-decrease-font-size
+        :n "H"  #'eww-back-url
+        :n "L"  #'eww-forward-url
+        :n "r"  #'eww-reload
+        :n "gt" #'eww-switch-to-buffer
+        :n "gT" #'eww-list-buffers
+        :n "gb" #'eww-list-bookmarks
+        :n "S" #'+eww-search-in-other-window
+        :n "o"  #'eww-search-words
+        :n "O"  #'+eww-visit-bookmark
+        :n "m"  #'+eww-bookmark-page
+        :n "D"  #'+eww-download-html
+        :n "C-O"#'+open-link-at-point-in-external-browser
+        :n "C-o"#'eww-browse-with-external-browser
+        :n "C-e"#'+eww-browse-dwim
+        :n "M"  #'browse-url-at-point-umpv
+        :n "f" #'+eww-jump-to-url-on-page
+        :n "C-f" #'+eww-visit-url-on-page
         [remap imenu] #'+eww-jump-to-url-on-page
 
         (:localleader
@@ -34,9 +54,50 @@
           :desc "readable" "r" #'eww-readable
           :desc "colors" "c" #'eww-toggle-colors
           :desc "fonts" "f" #'eww-toggle-fonts
-          :desc "images" "i" #'eww-toggle-images)
+          :desc "images" "i" #'eww-toggle-images))))
 
-         (:prefix ("y" . "copy")
-          :desc "copy url" "y" #'+eww-copy-current-url
-          :desc "copy for Org" "o" #'org-eww-copy-for-org-mode))))
+  ;; (map! :map eww-mode-map ;TODO add yank and kill history
 
+(use-package! goto-addr
+  :config
+  (setq goto-address-url-face 'link
+        goto-address-url-mouse-face 'highlight
+        goto-address-mail-face nil
+        goto-address-mail-mouse-face 'highlight))
+
+(use-package! shr
+  :config
+  (setq shr-use-colors nil             ; t is bad for accessibility
+        shr-use-fonts nil              ; t is not for me
+        shr-max-image-proportion 0.6
+        shr-image-animate nil          ; No GIFs, thank you!
+        shr-width fill-column          ; check `prot-eww-readable'
+        shr-max-width fill-column
+        shr-discard-aria-hidden t
+        shr-cookie-policy nil))
+
+(use-package! url-cookie
+  :config
+  (setq url-cookie-untrusted-urls '(".*")))
+
+(use-package! browse-url
+  :commands (browse-url-at-point-umpv browse-url-umpv)
+  :config
+  (when IS-LINUX
+    (defun browse-url-umpv (url &optional single)
+      (start-process "mpv" nil (if single "mpv" "mpv")
+                     (shell-quote-wildcard-pattern url)))
+
+    (defun browse-url-mpv (url)
+      (browse-url-umpv url t))
+
+    (defun browse-url-at-point-umpv (&optional single)
+      "Open link in mpv"
+      (interactive "P")
+      (let ((browse-url-browser-function
+             (if single
+                 (lambda (url &optional _new-window) (browse-url-umpv url t))
+               #'browse-url-umpv)))
+        (browse-url-at-point)))
+
+    ))
